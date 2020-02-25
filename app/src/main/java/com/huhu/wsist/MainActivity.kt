3c910abcb6a77b4a21b97dfc81a8998dfa74cf1c
NamDareum <namdalen1115@gmail.com>
2020-02-25 20:28:38 +0900

    Init: android project

---
+++ app/src/main/java/com/huhu/wsist/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.huhu.wsist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
