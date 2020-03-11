4c85deed8cf5c191be7a662095356f569075a580
NamDareum <namdalen1115@gmail.com>
2020-03-11 23:47:27 +0900

    [#3] flagment 추가
    Resolves #3

---
+++ app/src/main/java/com/huhu/wsist/ListFragment.kt
@@ -0,0 +1,14 @@
+package com.huhu.wsist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.list_fragment, container, false)
+    }
+}
--- app/src/main/java/com/huhu/wsist/MainActivity.kt
+++ app/src/main/java/com/huhu/wsist/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.huhu.wsist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private var _bottomNavigationView: BottomNavigationView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment()).commit()
+        }
+
+        _bottomNavigationView = findViewById(R.id.bottom)
+        _bottomNavigationView?.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_home -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment()).commit()
+                }
+                R.id.action_search -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, SearchFragment()).commit()
+                }
+                R.id.action_playlists -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, ListFragment()).commit()
+                }
+                R.id.action_settings -> {
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, SettingFragment()).commit()
+                }
+            }
+            true
+        })
+
+
     }
 }
