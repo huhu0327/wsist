fbecf7cc4b84fbfee0b6a435731ca0a6d88dc2c1
huhu0327 <ndr.developer@gmail.com>
2020-03-13 00:49:35 +0900

    Modify: refactoring switch flagment

--- app/src/main/java/com/huhu/wsist/main/MainActivity.kt
+++ app/src/main/java/com/huhu/wsist/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.huhu.wsist
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -12,29 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var transFragment: Fragment = HomeFragment()
+
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment()).commit()
+            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, transFragment as Fragment)
+                .commit()
         }
 
         _bottomNavigationView = findViewById(R.id.bottom)
         _bottomNavigationView?.setOnNavigationItemSelectedListener(BottomNavigationView.OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.action_home -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, HomeFragment()).commit()
-                }
-                R.id.action_search -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, SearchFragment()).commit()
-                }
-                R.id.action_playlists -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, ListFragment()).commit()
-                }
-                R.id.action_settings -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, SettingFragment()).commit()
-                }
+            val tempFragment: Fragment? = when (item.itemId) {
+                R.id.action_home -> HomeFragment()
+                R.id.action_search -> SearchFragment()
+                R.id.action_playlists -> ListFragment()
+                R.id.action_settings -> SettingFragment()
+                else -> null
             }
+
+            if (tempFragment != null && tempFragment != transFragment) {
+                transFragment = tempFragment
+                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, transFragment as Fragment)
+                    .commit()
+            }
+
             true
         })
-
 
     }
 }
